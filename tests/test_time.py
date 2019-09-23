322183bb341294d44df9b0d623e728f9f78e97cf
james5101 <james.c.noonan@gmail.com>
2019-09-22 22:32:21 -0400

    tried something didnt work

--- tests/test_time.py
+++ tests/test_time.py
@@ -1,14 +1,11 @@
-
-
+import pytest
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "../src")
-import src.time_print
 
-
-def test_time_now_should_not_throw():
-      src.time_print.return_date() 
     
 
 def test_hello():
     return "Hello"
+
+def test_by():
+    return "by"
